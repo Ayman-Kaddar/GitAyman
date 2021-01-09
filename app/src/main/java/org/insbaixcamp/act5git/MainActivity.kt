bd310603c6948cc03111df605128c6fcba6b363a
Ayman <ayman.kaddar@insbaixcamp.cat>
2021-01-09 21:14:36 +0100

    First Commit

---
+++ app/src/main/java/org/insbaixcamp/act5git/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.insbaixcamp.act5git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
