c69a529d768a2442e406c49553b4a9f0f143dbb8
Ayman <ayman.kaddar@insbaixcamp.cat>
2021-01-09 21:22:48 +0100

    Second Commit

--- app/src/main/java/org/insbaixcamp/act5git/MainActivity.kt
+++ app/src/main/java/org/insbaixcamp/act5git/MainActivity.kt
@@ -2,10 +2,13 @@ package org.insbaixcamp.act5git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("TAG","Esto es una Prueba")
     }
 }
